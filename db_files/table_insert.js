1798398797211025c8047ed793ce1f6e282d82a6
justkunz <Justine@Justine-Kunzs-MacBook-Pro.local>
2014-02-20 18:24:55 -0500

    Added more questions

--- db_files/table_insert.js
+++ db_files/table_insert.js
@@ -4,9 +4,23 @@ query.connectionParameters = "postgres://127.0.0.1:5432/charity_trivia";
 
 
 // insert one row into questions
-query("INSERT INTO questions(question, answer, fake_answer_1, fake_answer_2, fake_answer_3, charity_id, total_attempts, correct_attempts) values($1, $2, $3, $4, $5, $6, $7, $8)", ['Rhinos are most commonly poached for which feature?', 'horn', 'skin', 'feathers', 'urine', 1, 0, 0], function(err, rows, result) {
+query("INSERT INTO questions(question, answer, fake_answer_1, fake_answer_2, fake_answer_3, charity_id, total_attempts, correct_attempts) values($1, $2, $3, $4, $5, $6, $7, $8)", ['Rhinos are most commonly poached for which feature?', 'Horn', 'Skin', 'Feathers', 'Urine', 1, 0, 0], function(err, rows, result) {
       console.log(err);
 });
+
+query("INSERT INTO questions(question, answer, fake_answer_1, fake_answer_2, fake_answer_3, charity_id, total_attempts, correct_attempts) values($1, $2, $3, $4, $5, $6, $7, $8)", ['Which type of animal has the most extinct species?', 'Gastropods (Snails and Slugs)', 'Mammals', 'Reptiles', 'Amphibians', 1, 0, 0], function(err, rows, result) {
+      console.log(err);
+});
+
+query("INSERT INTO questions(question, answer, fake_answer_1, fake_answer_2, fake_answer_3, charity_id, total_attempts, correct_attempts) values($1, $2, $3, $4, $5, $6, $7, $8)", ['What portion of open ocean sharks are threatened with extinction?', '1/3', '1/2', '1/4', '1/5', 1, 0, 0], function(err, rows, result) {
+      console.log(err);
+});
+
+query("INSERT INTO questions(question, answer, fake_answer_1, fake_answer_2, fake_answer_3, charity_id, total_attempts, correct_attempts) values($1, $2, $3, $4, $5, $6, $7, $8)", ['What percent of coral reefs are rated as threatened?', '75%', '25%', '50%', '100%', 1, 0, 0], function(err, rows, result) {
+      console.log(err);
+});
+
+
 
 query("SELECT * FROM questions", function(err, rows, result) {
       console.log(rows);
